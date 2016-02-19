7fe4a5c6bcf32c42f0cfc23953e111339ac75abc
Andy Tu <andysctu@gmail.com>
2016-02-19 13:24:47 -0500

    json

--- db/mech.go
+++ db/mech.go
@@ -1,15 +1,15 @@
 package db
 
 type Mech struct {
-	Uid       int    `json:"uid"`
-	Arms      string `json:"arms"`
-	Legs      string `json:"legs"`
-	Core      string `json:"core"`
-	Head      string `json:"head"`
-	Weapon1L  int    `json:"weapon1l"`
-	Weapon1R  int    `json:"weapon1r"`
-	Weapon2L  int    `json:"weapon2l"`
-	Weapon2R  int    `json:"weapon2r"`
-	Booster   int    `json:"booster"`
-	IsPrimary bool   `json:"isPrimary"`
+	Uid       int    //`json:"uid"`
+	Arms      string //`json:"arms"`
+	Legs      string //`json:"legs"`
+	Core      string //`json:"core"`
+	Head      string //`json:"head"`
+	Weapon1L  int    //`json:"weapon1l"`
+	Weapon1R  int    //`json:"weapon1r"`
+	Weapon2L  int    //`json:"weapon2l"`
+	Weapon2R  int    //`json:"weapon2r"`
+	Booster   int    //`json:"booster"`
+	IsPrimary bool   //`json:"isPrimary"`
 }
